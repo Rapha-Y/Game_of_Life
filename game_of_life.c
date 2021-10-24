894486a33172f110dc34756c311761c1ee2ef028
Rapha-Y <raph.yuki@gmail.com>
2021-10-24 19:20:40 -0300

    Neighbor count function added

--- game_of_life.c
+++ game_of_life.c
@@ -9,37 +9,97 @@
 void init_grid(int **grid) {
     for (int i = 0; i < MAX_SIZE; i++) {
         for (int j = 0; j < MAX_SIZE; j++) {
-            grid[i][j] = 0;
+            grid[i][j] = DEAD;
         }
     }
 
     //Glider
-    int row = 1;
-    int col = 1;
-    grid[row][col + 1] = 1;
-    grid[row + 1][col + 2] = 1;
-    grid[row + 2][col] = 1;
-    grid[row + 2][col + 1] = 1;
-    grid[row + 2][col + 2] = 2;
+    int row = LIVING;
+    int col = LIVING;
+    grid[row][col + 1] = LIVING;
+    grid[row + 1][col + 2] = LIVING;
+    grid[row + 2][col] = LIVING;
+    grid[row + 2][col + 1] = LIVING;
+    grid[row + 2][col + 2] = LIVING;
 
     //R-pentonimo
     row = 10;
     col = 10;
-    grid[row][col + 1] = 1;
-    grid[row][col + 2] = 1;
-    grid[row + 1][col] = 1;
-    grid[row + 1][col + 1] = 1;
-    grid[row + 2][col + 1] = 1;
+    grid[row][col + 1] = LIVING;
+    grid[row][col + 2] = LIVING;
+    grid[row + 1][col] = LIVING;
+    grid[row + 1][col + 1] = LIVING;
+    grid[row + 2][col + 1] = LIVING;
+}
+
+int getNeighbors(int **grid, int i, int j) {
+    int prev_i = i - 1;
+    int prev_j = j - 1;
+    int next_i = i + 1;
+    int next_j = j + 1;
+
+    //loop through the board's edges
+    if (prev_i < 0) {
+        prev_i = MAX_SIZE - 1;
+    }
+    if (prev_j < 0) {
+        prev_j = MAX_SIZE - 1;
+    }
+    if (next_i > MAX_SIZE - 1) {
+        next_i = 0;
+    }
+    if (next_j > MAX_SIZE - 1) {
+        next_j = 0;
+    }
+
+    //count number of living neighbors
+    int neighbor_num = 0;
+    if (grid[prev_i][prev_j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[prev_i][j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[prev_i][next_j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[i][prev_j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[i][next_j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[next_i][prev_j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[next_i][j] == LIVING) {
+        neighbor_num++;
+    }
+    if (grid[next_i][next_j] == LIVING) {
+        neighbor_num++;
+    }
+
+    return neighbor_num;
 }
 
 void show_grid(int **mat) {
     for (int i = 0; i < MAX_SIZE; i++) {
         for (int j = 0; j < MAX_SIZE; j++) {
-            if (mat[i][j] == 0) {
+            if (mat[i][j] == DEAD) {
                 printf("%c", 32);
             } else {
                 printf("%c", 219);
             }
+        }
+        printf("\n");
+    }
+}
+
+void show_count(int **mat) {
+    for (int i = 0; i < MAX_SIZE; i++) {
+        for (int j = 0; j < MAX_SIZE; j++) {
+            int neighbor_num = getNeighbors(mat, i, j);
+            printf("%d", neighbor_num);
         }
         printf("\n");
     }
@@ -53,6 +113,7 @@ int main() {
 
     init_grid(grid);
     show_grid(grid);
+    show_count(grid);
 
     return 0;
 }
