8bd23f37e1cf64249d79fea7873d2d83d2634245
Rapha-Y <raph.yuki@gmail.com>
2021-10-25 11:05:16 -0300

    Multithreading added

--- game_of_life.c
+++ game_of_life.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 #define MAX_SIZE 2048
 #define GEN_NUM 2000
+#define THREAD_NUM 1
 #define LIVING 1
 #define DEAD 0
 
 void init_grid(int **grid) {
-    for (int i = 0; i < MAX_SIZE; i++) {
-        for (int j = 0; j < MAX_SIZE; j++) {
+    int i, j;
+    #pragma omp parallel for private(i, j) num_threads(THREAD_NUM)
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j < MAX_SIZE; j++) {
             grid[i][j] = DEAD;
         }
     }
@@ -83,8 +87,10 @@ int getNeighbors(int **grid, int i, int j) {
 }
 
 void fill_newgrid(int **grid, int **newgrid) {
-    for (int i = 0; i < MAX_SIZE; i++) {
-        for (int j = 0; j < MAX_SIZE; j++) {
+    int i, j;
+    #pragma omp parallel for private(i, j) num_threads(THREAD_NUM)
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j < MAX_SIZE; j++) {
             int neighbor_num = getNeighbors(grid, i, j);
 
             if (grid[i][j] == LIVING && (neighbor_num < 2 || neighbor_num > 3)) {
@@ -99,17 +105,21 @@ void fill_newgrid(int **grid, int **newgrid) {
 }
 
 void copy_newgrid(int **grid, int **newgrid) {
-    for (int i = 0; i < MAX_SIZE; i++) {
-        for (int j = 0; j < MAX_SIZE; j++) {
+    int i, j;
+    #pragma omp parallel for private(i, j) num_threads(THREAD_NUM)
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j < MAX_SIZE; j++) {
             grid[i][j] = newgrid[i][j];
         }
     }
 }
 
 int count_living(int **grid) {
+    int i, j;
     int living_num = 0;
-    for (int i = 0; i < MAX_SIZE; i++) {
-        for (int j = 0; j < MAX_SIZE; j++) {
+    #pragma omp parallel for private(i, j) reduction(+:living_num) num_threads(THREAD_NUM)
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j < MAX_SIZE; j++) {
             if (grid[i][j] == LIVING) {
                 living_num++;
             }
@@ -118,6 +128,7 @@ int count_living(int **grid) {
     return living_num;
 }
 
+//for visualization only
 void show_grid(int **mat) {
     for (int i = 0; i < MAX_SIZE; i++) {
         for (int j = 0; j < MAX_SIZE; j++) {
@@ -132,24 +143,33 @@ void show_grid(int **mat) {
 }
 
 int main() {
+    double start_time = omp_get_wtime();
+
+    int i;
     int **grid = (int**) malloc(MAX_SIZE * sizeof(int*));
-    for (int i = 0; i < MAX_SIZE; i++) {
+    #pragma omp parallel for private(i) num_threads(THREAD_NUM)
+    for (i = 0; i < MAX_SIZE; i++) {
         grid[i] = (int*) malloc(MAX_SIZE * sizeof(int));
     }
 
     int **newgrid = (int**) malloc(MAX_SIZE * sizeof(int*));
-    for (int i = 0; i < MAX_SIZE; i++) {
+    #pragma omp parallel for private(i) num_threads(THREAD_NUM)
+    for (i = 0; i < MAX_SIZE; i++) {
         newgrid[i] = (int*) malloc(MAX_SIZE * sizeof(int));
     }
-
+    
     init_grid(grid);
     printf("Initial state: %d\n", count_living(grid));
+    
 
     for (int i = 0; i < GEN_NUM; i++) {
         fill_newgrid(grid, newgrid);
         copy_newgrid(grid, newgrid);
         printf("Gen %d: %d\n", i + 1, count_living(grid));
     }
+
+    double end_time = omp_get_wtime();
+    printf("%f\n", end_time - start_time);
 
     return 0;
 }
