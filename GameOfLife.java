be019f9d1e547e9fe5c6745d6534bd89b8050be8
Rapha-Y <raph.yuki@gmail.com>
2021-10-26 16:59:03 -0300

    Grid initialization added (Java)

---
+++ GameOfLife.java
@@ -0,0 +1,60 @@
+public class GameOfLife {
+    private static final int MAX_SIZE = 50; //2048;
+    private static final int GEN_NUM = 5; //2000;
+    private static final int THREAD_NUM = 1;
+    private static final int LIVING = 1;
+    private static final int DEAD = 0;
+
+    private int grid[][];
+    private int newgrid[][];
+
+    private void fill_grids() {
+        this.grid = new int[this.MAX_SIZE][this.MAX_SIZE];
+        this.newgrid = new int[this.MAX_SIZE][this.MAX_SIZE];
+
+        for (int i = 0; i < this.MAX_SIZE; i++) {
+            for (int j = 0; j < this.MAX_SIZE; j++) {
+                this.grid[i][j] = this.DEAD;
+                this.newgrid[i][j] = this.DEAD;
+            }
+        }
+
+        //Glider
+        int row = 1;
+        int col = 1;
+        this.grid[row][col + 1] = this.LIVING;
+        this.grid[row + 1][col + 2] = this.LIVING;
+        this.grid[row + 2][col] = this.LIVING;
+        this.grid[row + 2][col + 1] = this.LIVING;
+        this.grid[row + 2][col + 2] = this.LIVING;
+
+        //R-pentonimo
+        row = 10;
+        col = 30;
+        grid[row][col + 1] = this.LIVING;
+        grid[row][col + 2] = this.LIVING;
+        grid[row + 1][col] = this.LIVING;
+        grid[row + 1][col + 1] = this.LIVING;
+        grid[row + 2][col + 1] = this.LIVING;
+    }
+
+    //for visualization only
+    private void show_grid() {
+        for (int i = 0; i < this.MAX_SIZE; i++) {
+            for (int j = 0; j < this.MAX_SIZE; j++) {
+                if (this.grid[i][j] == this.DEAD) {
+                    System.out.print('D');
+                } else {
+                    System.out.print('L');
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        GameOfLife game = new GameOfLife();
+        game.fill_grids();
+        game.show_grid();
+    }
+}
